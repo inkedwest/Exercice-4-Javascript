4e33eac3efcb9c084b77b13a13fbe3d3f91b3770
Glatigny <f.west@live.fr>
2019-01-21 14:27:37 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+function getHU()
+{
+             var lastname = document.getElementById("lastname").value;
+             var firstname = document.getElementById("firstname").value;
+             var city = document.getElementById("city").value;
+ alert('nom :' + lastname +
+      '\nprénom :' + firstname +
+      '\nville :' + city);
+
+}
