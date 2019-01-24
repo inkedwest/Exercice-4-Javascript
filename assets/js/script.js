b7d10bd8ca2aee3d22f901840dcde05eb602a3f5
Glatigny <f.west@live.fr>
2019-01-24 11:27:43 +0100

    Correction exercice 4 Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
-
+// on crée une fonction displayInformations que l'on va déclencher au clic pour recuperer et afficher la valeur de nos "inputs"
 function displayInformations(){
-             var lastname = document.getElementById('lastname').value; // var sert à stocker les infos / documents. est un appel de l'HTML
-             var firstname = document.getElementById('firstname').value;
-             var city = document.getElementById('city').value;
-
- alert('Nom :' + lastname + '\nPrénom :' + firstname + '\nVille :' + city); //alert pour appeler l'affichae ; en vert, pour l'affichage à l'écran ; firstname = appel de l'ID
-
+  // Ici je declare mon regex qui va permettre de securiser le code
+  var regexName = /^[a-zA-Z ]+$/;
+  // dans le document HTML (document) tu recupere l'element grace à son ID =>(getElementById) ('ex :lastname)
+  // ici on ne recupere que la valeur de l'element grace à .value
+  // on stocke la valeur recuperée dans une variable (avec var etc ...)
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  // avec if je declare la condition qui permettra de retourner un message selon ce que j'ai entré dans les cases
+  if (lastname.match(regexName) && firstname.match(regexName) && city.match(regexName)){
+    alert('Nom : ' + lastname + '\nPrénom : ' + firstname + '\nVille : ' + city);
+  // Sinon la boite de dialogue affichera que ce n'est pas autorisé
+  }else {
+    alert('Ceci n'est pas autorisé');
+  }
 }
